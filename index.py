ea8a65f02c3d2a599bf0562d6fe9c7ff22cf8988
nick <nickdlk@163.com>
2024-05-11 00:17:48 +0800

    fix: test start_server

--- index.py
+++ index.py
@@ -2,4 +2,8 @@ import pywebio
 
 from network_snip import main
 
-pywebio.platform.flask.start_server(main)
+app = pywebio.platform.flask.wsgi_app(main)
+
+if __name__ == '__main__':
+    print("app start")
+    app.run(debug=True)
