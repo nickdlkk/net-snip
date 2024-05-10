cbef9377a552e3d1f9f8731077216e61d3bee7fc
nick <nickdlk@163.com>
2024-05-11 00:03:20 +0800

    fix: test server

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
-import pywebio
+# import pywebio
+from pywebio.platform.fastapi import asgi_app
 
 from network_snip import main
 
-handler =  pywebio.platform.flask.wsgi_app(main)
+app = asgi_app(main)
+# handler =  pywebio.platform.flask.wsgi_app(main)
